3aa3e1f68e5dc030522c6c35fa337c5abd9785cd
benjaminstern <benjamin.matteo.stern@gmail.com>
2016-06-28 12:17:22 +0200

    Klassen ergänzt

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,9 +1,32 @@
 package sample;
 
+import java.util.ArrayList;
+
 /**
  * Created by Benjamin on 28.06.2016.
  */
 public class Game {
+    public static boolean player1 = true;
+    public static boolean player2 = false;
+    private boolean currentPlayer = player1;
 
+    public boolean checkForMill(){
+        boolean mill = false;
 
+        //prüfen ob der currentPlayer eine NEUE Mühle gebildet hat
+
+        return mill;
+    }
+
+    public void changeGameStatus(){
+
+    }
+
+    public boolean allowedToRemovePiece(Piece piece){
+        boolean isAllowed = true;
+
+        //abfrage Logik
+
+        return isAllowed;
+    }
 }
--- src/sample/Tournament.java
+++ src/sample/Tournament.java
@@ -1,7 +1,10 @@
 package sample;
 
+import java.util.ArrayList;
+
 /**
  * Created by Benjamin on 28.06.2016.
  */
 public class Tournament {
+    private ArrayList<Player> players = new ArrayList<>();
 }
