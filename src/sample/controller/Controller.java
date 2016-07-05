9cb77c7e2600d9aa67731fbf56138526498b034d
colin1886 <colin.hauri@hotmail.com>
2016-07-05 14:55:08 +0200

    Nach dem Buttonklick von 1vs1 öffnet sich das Namen Fenster. Gleichzeitig wird das Startfenster geschlossen

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -1,21 +1,53 @@
 package sample.controller;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import sample.view.View;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller  {
+public class Controller implements Initializable{
+    private Stage primaryStage;
 
-    public void open1vs1(ActionEvent event) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/1vs1.fxml"));
-        Parent root = (Parent) fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root));
-        stage.show();
+    public Controller(Stage stage){
+        primaryStage = stage;
+    }
+
+    @FXML
+    private Button button1;
+
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        button1.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample/view/1vs1.fxml"));
+                Parent root = null;
+                try {
+                    primaryStage.close();
+                    root = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
     }
+
+
 
 }
--- src/sample/view/View.java
+++ src/sample/view/View.java
@@ -4,18 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import sample.controller.Controller;
 
 public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/start.fxml"));
+        Controller controller = new Controller(primaryStage);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample/view/start.fxml"));
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
         primaryStage.setTitle("Mühle");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
 
+
     }
+
+
+
 
     public static void main(String[] args) {
         launch(args);
