35a7592908d53279be73cb1ddb38ac5235b2337f
Stuber Gregor <gregor98@sunrise.ch>
2016-07-05 12:38:00 +0200

    changed main.java

--- src/sample/view/View.java
+++ src/sample/view/View.java
@@ -1,7 +1,24 @@
 package sample.view;
 
-/**
- * Created by Benjamin on 28.06.2016.
- */
-public class View {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/start.fxml"));
+        primaryStage.setTitle("Mühle");
+        primaryStage.setScene(new Scene(root, 800, 400));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
