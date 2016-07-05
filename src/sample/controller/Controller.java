f5c4f4f9692d3c03626326e3d6fc48859831a580
colin1886 <colin.hauri@hotmail.com>
2016-07-05 12:14:50 +0200

    Fehlerbehebung Pfad start.fxml

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -1,13 +1,20 @@
 package sample.controller;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 
-public class Controller {
+public class Controller  {
 
-    public void open1vs1(ActionEvent event){
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("1vs1.fxml"));
+    public void open1vs1(ActionEvent event) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/1vs1.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
 
     }
 
