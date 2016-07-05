7b4b9067c51f2fef70e1350fdfa973b5c8f881b2
benjaminstern <benjamin.matteo.stern@gmail.com>
2016-07-05 16:21:21 +0200

    movePiece

--- src/sample/model/Point.java
+++ src/sample/model/Point.java
@@ -4,21 +4,7 @@ package sample.model;
  * Created by Benjamin on 28.06.2016.
  */
 public class Point {
-    private Boolean occupied;
     private Piece piece;
-
-    public Boolean getOccupied() {
-        return occupied;
-    }
-
-    public void setOccupied(Boolean occupied) {
-        this.occupied = occupied;
-    }
-
-    //Verschiebt den Spielstein
-    public void movePiece(Piece piece){
-
-    }
 
     public Piece getPiece() {
         return piece;
@@ -26,5 +12,9 @@ public class Point {
 
     public void setPiece(Piece piece) {
         this.piece = piece;
+    }
+
+    public void removePiece() {
+        piece = null;
     }
 }
